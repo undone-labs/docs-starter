60b7db0407071c088ccef3e247fd921dd9f526e1
tikagan <30419893+tikagan@users.noreply.github.com>
2023-04-13 16:57:38 -0400

    feat: index redirects to /introduction

---
+++ packages/fe/pages/introduction.vue
@@ -0,0 +1,93 @@
+<template>
+  <div :class="`page page-${tag} container`">
+
+    <div class="grid">
+      <div class="col-3">
+        <Sidebar />
+      </div>
+      <div class="col-9">
+        <div class="page-content">
+
+          <MarkdownParser :markdown="markdown" />
+
+          <MagellanMenu :links="magellanLinks" />
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// ===================================================================== Imports
+import { mapGetters } from 'vuex'
+
+import Sidebar from '@/components/sidebar'
+import MarkdownParser from '@/components/markdown-parser'
+import MagellanMenu from '@/components/magellan-menu'
+
+import IndexPageData from '@/content/pages/index.json'
+import IndexPageContent from '@/content/markdown/index.md'
+
+// ====================================================================== Export
+export default {
+  name: 'IndexPage',
+
+  components: {
+    Sidebar,
+    MarkdownParser,
+    MagellanMenu
+  },
+
+  data () {
+    return {
+      tag: 'index'
+    }
+  },
+
+  async fetch ({ app, store }) {
+    await store.dispatch('general/getBaseData', { key: 'index', data: IndexPageData })
+  },
+
+  head () {
+    return this.$compileSeo(this.$getSeo(this.tag))
+  },
+
+  computed: {
+    ...mapGetters({
+      siteContent: 'general/siteContent'
+    }),
+    generalPageData () {
+      return this.siteContent.general
+    },
+    pageData () {
+      return this.siteContent[this.tag].page_content
+    },
+    markdown () {
+      return IndexPageContent
+    },
+    magellanLinks () {
+      const regex = /(?<=#{2})[\w \p{P}]+(?=\n)/gu
+      const headings = this.markdown.match(regex)
+      return headings
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// ///////////////////////////////////////////////////////////////////// General
+.page-index {
+  position: relative;
+  margin-top: $siteHeaderHeight;
+}
+
+.page-content {
+  position: relative;
+  width: 100%;
+  display: flex;
+  margin: 3.75rem 0 2rem 0;
+}
+
+</style>
