c12339c77a903a65f89433a560e6f1408444d041
Nauras Jabari <nauras@agencyundone.com>
2023-09-25 11:14:30 -0400

    feat: abstracted site logo

--- packages/src/components/site-header.vue
+++ packages/src/components/site-header.vue
@@ -3,11 +3,7 @@
 
     <!-- ============================================================== Logo -->
     <NuxtLink to="/docs" class="logo-link">
-      <component
-        :is="useResolveDoczillaComponent('logo')"
-        v-if="useResolveDoczillaComponent('logo')"
-        class="logo" />
-      <Logo v-else class="logo" />
+      <component :is="useResolveDoczillaComponent('site-logo')" class="logo" />
     </NuxtLink>
 
     <!-- ========================================================= Nav links -->
