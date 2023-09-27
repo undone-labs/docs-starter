c5009672176e14ef267c9523e6c8eb6bb7d1be86
Nauras Jabari <nauras@agencyundone.com>
2023-09-27 11:24:29 -0400

    fix: sitemap no longer generates src.md

--- packages/src/server/api/_sitemap-urls.js
+++ packages/src/server/api/_sitemap-urls.js
@@ -8,7 +8,8 @@ export default cachedEventHandler(async (e) => {
   let docs = await serverQueryContent(e).find()
   return docs.reduce((acc, doc) => {
     const path = doc._path
-    if (path && path.includes('/docs/content')) {
+    const file = doc._file
+    if (path && path.includes('/docs/content') && !file.includes('/src.md')) {
       acc.push({
         loc: path.replace('/docs/content', ''),
         lastmod: new Date()
